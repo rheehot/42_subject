62d63e309b951d18168dc2d42c1bdb58eb522427
mimseong <parkseongmin98@gmail.com>
2020-07-30 14:20:51 +0900

    ft_parse norm 체크

--- ft_parse.c
+++ ft_parse.c
@@ -13,7 +13,7 @@
 #include "libft.h"
 #include <stdio.h>
 
-void	pass_num(const char *str, size_t *idx, t_list *node, int is_width)
+void		pass_num(const char *str, size_t *idx, t_list *node, int is_width)
 {
 	int ans;
 
@@ -40,73 +40,72 @@ void	pass_num(const char *str, size_t *idx, t_list *node, int is_width)
 		node->length = ans;
 }
 
-void	parse_init_node(t_list *node)
+static void	check(t_send1 s, t_list *n, t_list **start)
 {
-	node->flag = 0;
-	node->width = 0;
-	node->precision = 0;
-	node->length = 0;
-	node->specifier = 0;
-	node->next = 0;
-	node->s_width = 0;
-	node->s_length = 0;
+	if (*(s.state) && is_flag((s.str)[*(s.i)]) && s.flag)
+	{
+		if (n->flag == 0)
+			n->flag = (s.str)[*(s.i)];
+		else if ((s.str)[*(s.i)] == '-')
+			n->flag = (s.str)[*(s.i)];
+	}
+	else if (*(s.state) && is_width((s.str)[*(s.i)]) && n->precision == 0)
+	{
+		pass_num((s.str), s.i, n, 1);
+		*(s.i) -= 1;
+	}
+	else if (*(s.state) && is_precision((s.str)[*(s.i)]))
+		n->precision = (s.str)[*(s.i)];
+	else if (*(s.state) && is_length((s.str)[*(s.i)]) && n->precision != 0)
+	{
+		pass_num((s.str), s.i, n, 0);
+		*(s.i) -= 1;
+	}
+	else if (*(s.state) && is_specifier((s.str)[*(s.i)]))
+	{
+		n->specifier = (s.str)[*(s.i)];
+		make_node(start, *n);
+		*(s.state) = 0;
+	}
 }
 
-void	make_node(t_list **start, t_list set_node)
+static void	check2(t_send1 s, t_list *n, int *flag)
 {
-	t_list *tmp;
-
-	tmp = ft_lstnew(set_node);
-	ft_lstadd_back(start, tmp);
+	if (*(s.state) == 0 && (s.str)[*(s.i)] == '%')
+	{
+		*(s.state) = 1;
+		parse_init_node(n);
+		*(s.i) += 1;
+		*flag = 1;
+		return ;
+	}
+	if (is_flag((s.str)[*(s.i)]) == 0)
+		*flag = 0;
 }
 
-void	ft_parse(const char *str, t_list **start)
+void		ft_parse(const char *str, t_list **start)
 {
 	size_t	i;
 	int		state;
 	int		flag;
 	t_list	set_node;
+	t_send1	send;
 
 	i = 0;
 	state = 0;
 	flag = 0;
 	while (str[i] != 0)
 	{
-		if (state == 0 && str[i] == '%')
-		{
-			state = 1;
-			parse_init_node(&set_node);
-			i++;
-			flag = 1;
-			continue ;
-		}
-		if (is_flag(str[i]) == 0)
-			flag = 0;
-		if (state && is_flag(str[i]) && flag)
-		{
-			if (set_node.flag == 0)
-				set_node.flag = str[i];
-			else if (str[i] == '-')
-				set_node.flag = str[i];
-		}
-		else if (state && is_width(str[i]) && set_node.precision == 0)
-		{
-			pass_num(str, &i, &set_node, 1);
-			continue ;
-		}
-		else if (state && is_precision(str[i]))
-			set_node.precision = str[i];
-		else if (state && is_length(str[i]) && set_node.precision != 0)
-		{
-			pass_num(str, &i, &set_node, 0);
-			continue ;
-		}
-		else if (state && is_specifier(str[i]))
-		{
-			set_node.specifier = str[i];
-			make_node(start, set_node);
-			state = 0;
-		}
+		send.state = &state;
+		send.str = str;
+		send.i = &i;
+		send.flag = flag;
+		check2(send, &set_node, &flag);
+		send.state = &state;
+		send.str = str;
+		send.i = &i;
+		send.flag = flag;
+		check(send, &set_node, start);
 		i++;
 	}
 }
