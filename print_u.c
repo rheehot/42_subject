522bff83192883b7a66d2cbff34e544be59704a8
mimseong <parkseongmin98@gmail.com>
2020-07-30 16:23:57 +0900

    print_d 놂 확인 완료

--- print_u.c
+++ print_u.c
@@ -12,62 +12,72 @@
 
 #include "libft.h"
 
-int	print_u(t_list node, unsigned int num)
+static void	check1(t_list *node, unsigned int num, char **print)
 {
-	char	*print;
+	if (node->length == 0 && num == 0 && node->precision == '.')
+		*print = make_zero();
+	else
+		*print = ft_undifuitoa(num);
+	if (node->length < 0)
+	{
+		node->length = 0;
+		node->precision = 0;
+	}
+}
+
+static void	check2(t_list *node, unsigned int num, t_send2 s)
+{
+	char	*p;
+
+	check1(node, num, &p);
+	*s.printc = ft_max(ft_strlen(p), node->length);
+	if (num < 0)
+		*s.printw = ft_max(*s.printc + 1, node->width);
+	else
+		*s.printw = ft_max(*s.printc, node->width);
+	if (node->flag == '0' && node->precision != '.')
+	{
+		if (num < 0)
+			*s.printc = *s.printw - 1;
+		else
+			*s.printc = *s.printw;
+	}
+	*s.strc = (char *)malloc(*s.printc);
+	*s.strw = (char *)malloc(*s.printw + 1);
+	if (node->flag == '0' || node->length > 0)
+		set_zero(*s.strc, *s.printc);
+	else
+		ft_setspace(*s.strc, *s.printc + 1);
+	ft_setspace(*s.strw, *s.printw + 1);
+	(*s.strw)[*s.printw] = 0;
+	ft_memmove(&((*s.strc)[*s.printc - ft_strlen(p)]), p, ft_strlen(p));
+	free(p);
+}
+
+static void	check3(t_list node, t_send2 s)
+{
+	if (node.flag == '-')
+		ft_memmove(&((*s.strw)[0]), *s.strc, *s.printc);
+	else
+		ft_memmove(&((*s.strw)[*s.printw - *s.printc]), *s.strc, *s.printc);
+	free(*s.strc);
+	ft_putstr_fd(*s.strw, 1);
+	free(*s.strw);
+}
+
+int			print_u(t_list node, unsigned int num)
+{
+	t_send2	send;
 	size_t	print_container;
 	size_t	print_width;
 	char	*str_container;
 	char	*str_width;
 
-	if (node.length == 0 && num == 0 && node.precision == '.')
-		print = make_zero();
-	else
-		print = ft_undifuitoa(num);
-	
-	if (node.length < 0)
-	{
-		node.length = 0;
-		node.precision = 0;
-	}
-
-	print_container = ft_max(ft_strlen(print), node.length);
-	if (num < 0)
-		print_width = ft_max(print_container + 1, node.width);
-	else
-		print_width = ft_max(print_container, node.width);
-
-	if (node.flag == '0' && node.precision != '.')
-	{
-		if (num < 0)
-			print_container = print_width - 1;
-		else
-			print_container = print_width;
-	}
-
-	//printf("print_container %zu print_width %zu", print_container, print_width);
-
-	str_container = (char *)malloc(print_container);
-	str_width = (char *)malloc(print_width + 1);
-	if (node.flag == '0' || node.length > 0)
-	{
-		set_zero(str_container, print_container);
-	}
-	else
-		ft_setspace(str_container, print_container + 1);
-	ft_setspace(str_width, print_width + 1);
-	str_width[print_width] = 0;
-
-	ft_memmove(&str_container[print_container - ft_strlen(print)], print, ft_strlen(print));
-	free(print);
-	if (node.flag == '-')
-		ft_memmove(&str_width[0], str_container, print_container);
-	else
-		ft_memmove(&str_width[print_width - print_container], str_container, print_container);
-	free(str_container);
-
-	ft_putstr_fd(str_width, 1);
-	free(str_width);
-
+	send.printc = &print_container;
+	send.printw = &print_width;
+	send.strc = &str_container;
+	send.strw = &str_width;
+	check2(&node, num, send);
+	check3(node, send);
 	return (print_width);
 }
